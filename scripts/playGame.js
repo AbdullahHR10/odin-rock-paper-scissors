e5df58043e291a855ace8047aadf01127b1726bf
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2025-08-28 19:58:37 +0300

    Add playGame function to play 5 rounds per game

---
+++ scripts/playGame.js
@@ -0,0 +1,38 @@
+function playGame() {
+  let humanPoints = 0;
+  let computerPoints = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    const beats = {
+      "rock": "scissors",
+      "scissors": "paper",
+      "paper": "rock"
+    }
+    if (humanChoice === computerChoice) {
+      console.log("It's a tie.");
+    }
+    else if (humanChoice in beats && beats[humanChoice] === computerChoice) {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+      humanPoints++;
+    }
+    else {
+      console.log(`Computer wins! ${computerChoice} beats ${humanChoice}`)
+      computerPoints++;
+    }
+  }
+  for (let i = 0; i < 5; i++) {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+  }
+
+  if (humanPoints > computerPoints) {
+    console.log("You win the game!");
+  } else if (computerPoints > humanPoints) {
+    console.log("Computer wins the game!");
+  } else {
+    console.log("The game is a tie!");
+  }
+}
+
+playGame();
